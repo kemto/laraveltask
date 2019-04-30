830be8717d90851bb2e3e1e4eda093d6e64da666
kemto <kemto.macbook@gmail.com>
2019-04-30 13:43:32 +0000

    tasklara11

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin¥NewsController@add');
-    Route::get('profile/create', 'Admin¥NewsController@add');
-    Route::get('profile/edit', 'Admin¥NewsController@edit');
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
 /*課題*/
 Route::group(['prefix' => 'admin'],function() {
-    Route::get('/XXX', 'Admin¥AAAController@bbb');
+    Route::get('/XXX', 'Admin\AAAController@bbb');
 });
